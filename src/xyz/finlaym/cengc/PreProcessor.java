71c0aa6b42e831e1ca7e78705f6eff2d2f28e95c
finlaywashere <finman292004@protonmail.com>
2023-11-18 09:33:21 -0500

    Added base XML parsing

--- src/xyz/finlaym/cengc/PreProcessor.java
+++ src/xyz/finlaym/cengc/PreProcessor.java
@@ -9,14 +9,18 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class PreProcessor {
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 		try {
 			new PreProcessor(new File("test_data/TA1.xml"));
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		long elapsed = System.currentTimeMillis()-start;
+		System.out.println("Time elapsed: "+ (elapsed/1000));
 	}
 	public PreProcessor(File xml) throws Exception{
 		// XML parsing was helped by https://www.baeldung.com/java-xerces-dom-parsing
@@ -26,5 +30,13 @@ public class PreProcessor {
 		Element graph = doc.getDocumentElement();
 		List<Node> nodes = new ArrayList<Node>();
 		List<Way> ways = new ArrayList<Way>();
+		NodeList nodesList = graph.getElementsByTagName("node");
+		NodeList waysList = graph.getElementsByTagName("way");
+		for(int i = 0; i < nodesList.getLength(); i++) {
+			org.w3c.dom.Node node = nodesList.item(i);
+		}
+		for(int i = 0; i < waysList.getLength(); i++) {
+			org.w3c.dom.Node way = waysList.item(i);
+		}
 	}
 }
